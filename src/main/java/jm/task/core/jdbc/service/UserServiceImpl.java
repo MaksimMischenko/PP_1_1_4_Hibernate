bf188158055de3881f08b85c4b2fbb1ac588bca8
MaksimMischenko <t89969534097@gmail.com>
2022-07-15 22:28:00 +0400

    1.1.4

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,16 +1,14 @@
 package jm.task.core.jdbc.service;
 
-// внутри класса создаём экземпляр UserDaoJDBCImpl (дальше сообразите что делать, идея поможет)))
-
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDao userDao = new UserDaoJDBCImpl();
-
+    private final UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
     public void createUsersTable() {
 
         userDao.createUsersTable();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,23 +1,47 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-
-    private static final String HOST = "jdbc:mysql://localhost:3306/sys";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "11111111";
-
     public static Connection getConnection() {
-
-        Connection connection = null;
         try {
-            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "11111111");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return connection;
+    }
+
+    private Util() {}
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/sys");
+                properties.put(Environment.USER, "root");
+                properties.put(Environment.PASS, "11111111");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
